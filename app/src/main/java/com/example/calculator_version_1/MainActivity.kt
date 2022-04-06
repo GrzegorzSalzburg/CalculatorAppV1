a72d21941b259c50d215b819e5a41ffcbab21312
Grzegorz Salzburg <252912@student.pwr.edu.pl>
2022-04-06 02:56:03 +0200

    Modifications

--- app/src/main/java/com/example/calculator_version_1/MainActivity.kt
+++ app/src/main/java/com/example/calculator_version_1/MainActivity.kt
@@ -3,34 +3,59 @@ package com.example.calculator_version_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var worktv: TextView
+    private lateinit var equationtv: TextView
     private lateinit var resulttv: TextView
     private var canAddOperation = false
     private var canAddDecimal = true
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        worktv = findViewById(R.id.worktv)
+        equationtv = findViewById(R.id.equationtv)
         resulttv = findViewById(R.id.resulttv)
     }
 
     fun allClearAction(view: View) {
-        worktv.text=""
-        resulttv.text=""
+        equationtv.text = ""
+        resulttv.text = ""
     }
+
     fun backSpaceAction(view: View) {
-        val length = worktv.length()
-        if(length > 0)
-            worktv.text = worktv.text.subSequence(0, length - 1)
+        val length = equationtv.length()
+        if (length > 0)
+            equationtv.text = equationtv.text.subSequence(0, length - 1)
     }
+
+    fun numberAction(view: View) {
+        if (view is Button) {
+            if (view.text == ".") {
+                if (canAddDecimal)
+                    equationtv.append(view.text)
+
+                canAddDecimal = false
+            } else
+                equationtv.append(view.text)
+
+            canAddOperation = true
+        }
+    }
+
+    fun operationAction(view: View){
+        if(view is Button && canAddOperation)
+        {
+            equationtv.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+
     fun equalsAction(view: View) {
 
     }
